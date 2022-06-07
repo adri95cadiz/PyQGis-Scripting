c4d05ad1e6782b23b398249c616e9639c68b8dc3
Adrián Portillo Sánchez <adri95cadiz@gmail.com>
2022-06-07 19:12:06 +0200

    Add files via upload

---
+++ split_merge_and_compare.py
@@ -0,0 +1,76 @@
+from qgis.core import *
+
+################################PARAMETERS#####################################
+#
+# NAME OF THE LAYER TO PROCESS:
+input_layer = 'INPUT_LAYER_NAME';
+# .SHP FILE WHERE THE RESULTS WILL BE STORED:
+result_file = "Path/To/File"
+# THRESHOLD VALUE TO COMPARE THE LAYERS:
+min_value = 1000;
+#
+###############################################################################
+
+# Obtenemos la capa relativa a las provincias a analizar.
+provincia = QgsProject.instance().mapLayersByName(capa_provincia)[0];
+
+###############################################################################
+
+# FILTER BY: PARAM = VAL
+
+###############################################################################
+
+# Select rows which have PARAM equals VAL (PARAM = VAL)
+selection_layer = processing.run('qgis:selectbyattribute',{'FIELD':'PARAM', 'INPUT':input_layer, 'METHOD':0, 'OPERATOR':0, 'VALUE':'VAL'})['OUTPUT'];
+
+# Copy the selection to a temporary layer.
+layer_selected = processing.run('qgis:saveselectedfeatures', {'INPUT':selection_layer, 'OUTPUT':'TEMPORARY_OUTPUT'})['OUTPUT'];
+
+# Iterate by CODES defined by ITER_PARAM.
+index_list = provincia_OV.fields().indexOf('ITER_PARAM');
+codes = provincia_OV.uniqueValues(index_list);
+
+###############################################################################
+
+array_layers_total = [];
+array_layers_value = [];
+
+# Para each code:
+for code in codes:
+    # Select rows which ITER_PARAM equals code.
+    selection = processing.run('qgis:selectbyattribute',{'FIELD':'CD_MUN', 'INPUT':layer_selected, 'METHOD':0, 'OPERATOR':0, 'VALUE':code})['OUTPUT'];
+    
+    # Copiamos la selección a una nueva capa temporal.
+    selected = processing.run('qgis:saveselectedfeatures', {'INPUT':selection, 'OUTPUT':'TEMPORARY_OUTPUT'})['OUTPUT'];
+    
+    # Disolvemos los municipios por codigo de municipio para obtener las unidades municipales.
+    layer_dissolve = processing.run("native:dissolve",{'INPUT':selected, 'FIELD':[], 'OUTPUT':'memory:'})['OUTPUT'];
+    
+    array_layers_total.append(layer_dissolve);
+    
+    # Calculate the total of value VAL_TOTAL.
+    list_values = QgsVectorLayerUtils.getValues(selected, 'VAL_TOTAL')[0];
+    list_values = list(filter(None, list_values));
+    total_value = sum(list_values);
+    
+    # If the total value is bigger than the min_value...
+    if total_value >= min_value:
+        array_layers_value.append(layer_dissolve);
+
+# We merge the layers from all the total layers array
+merge_total = processing.run("qgis:mergevectorlayers", {'LAYERS':array_layers_total,'CRS':'EPSG:25830','OUTPUT':'memory:'})['OUTPUT'];
+
+# We store the layer in the project with a new name
+merge_total.setName(input_layer + "_total_dissolved");
+result = result_file + input_layer + "_total_dissolved.shp";
+_writer = QgsVectorFileWriter.writeAsVectorFormat(merge_total,result,'UTF-8',merge_total.crs(),"ESRI Shapefile");
+QgsProject.instance().addMapLayer(merge_total);
+
+# We merge the layers from array where the value is bigger than the threshold
+merge_value = processing.run("qgis:mergevectorlayers", {'LAYERS':array_layers_value,'CRS':'EPSG:25830','OUTPUT':'memory:'})['OUTPUT'];
+
+# We store the layer in the project with a new name
+merge_value.setName(input_layer + "_value_dissolved");
+result = result_file + input_layer + "_value_dissolved.shp";
+_writer = QgsVectorFileWriter.writeAsVectorFormat(merge_value,result,'UTF-8',merge_value.crs(),"ESRI Shapefile");
+QgsProject.instance().addMapLayer(merge_value);
